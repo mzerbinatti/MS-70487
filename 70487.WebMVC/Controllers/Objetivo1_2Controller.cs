fbd52adb51eef8a58ae64ef29878d01da64828f9
Michel Zerbinati <mzerbinatti@yahoo.com.,br>
2017-07-03 18:46:11 -0300

    Exercicio CacheDependency de arquivos

--- 70487.WebMVC/Controllers/Objetivo1_2Controller.cs
+++ 70487.WebMVC/Controllers/Objetivo1_2Controller.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Caching;
+using System.IO;
 
 namespace _70487.WebMVC.Controllers
 {
@@ -11,6 +13,21 @@ namespace _70487.WebMVC.Controllers
         // GET: Objetivo1_2
         public ActionResult Index()
         {
+            
+            var fileInfo = new FileInfo(Server.MapPath("~/Content/Objetivo1_2/sampleFile.txt"));
+            Cache cache = new Cache();
+            var fileContent = cache["sampleFile"];
+            
+            if(fileContent == null || string.IsNullOrEmpty(fileContent.ToString()))
+            {
+                using (StreamReader sr = new StreamReader(fileInfo.FullName))
+                {
+                    fileContent = sr.ReadToEnd();
+                    CacheDependency cacheDependency = new CacheDependency(fileInfo.FullName);
+                    cache.Insert("sampleFile", fileContent, cacheDependency);
+                }
+            }
+            ViewBag.fileContent = fileContent.ToString();
             return View();
         }
     }
