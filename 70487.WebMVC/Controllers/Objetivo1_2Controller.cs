ce0511c59acab8679a9c66236eafe3b3f4518a5f
Michel Zerbinati <Michel Zerbinati>
2017-07-03 17:38:19 -0300

    Add project files.

---
+++ 70487.WebMVC/Controllers/Objetivo1_2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _70487.WebMVC.Controllers
+{
+    public class Objetivo1_2Controller : Controller
+    {
+        // GET: Objetivo1_2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 70487.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_70487.WebMVC.MvcApplication" Language="C#" %>
